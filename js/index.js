4cf4073c206dd2210abd04bfc8ffbe1e690545fa
Zeb Pykosz <zeb.apy@gmail.com>
2017-09-26 15:36:29 -0400

    Update 2017-09-26T19:36:28.161Z

--- js/index.js
+++ js/index.js
@@ -1,26 +1,7 @@
-// $( document ).ready(function() {
-
-	// $( ".menu-close" ).hide();
-	// 	// $( ".menu" ).hide();
-	// 	$( ".menu-open" ).click(function() {
-	// 		$( ".menu" ).slideToggle( 250, function() {
-	// 		$( ".menu-open" ).hide();
-	// 		$( ".menu-close" ).show();
-	// 		});
-	// 	});
-
-	// $( ".menu-close" ).click(function() {
-	// 	$( ".menu" ).slideToggle( 250, function() {
-	// 		$( ".menu-close" ).hide();
-	// 		$( ".menu-open" ).show();
-	// 		$(".menu-open").css("display","");
-	// 	});
-	// });
-
-// });
-//on click of menu hamburger
-$(".menu-open").click(function(){
-  //expands menu
-  $(".nav-primary").toggleClass("active-menu");
-
-})
+(function() {
+  var menuBtn = document.querySelector('.menu-open');
+  var nav = document.querySelector('.nav-primary');
+  menuBtn.addEventListener('click', function(event) {
+    nav.classList.toggle('active-menu');
+  });
+})();
